9cf028e5bdf4506d149415ad554c2e3009a0122b
Vaishnavi_S_Krishnan <66055764+Vaishnavi0123@users.noreply.github.com>
2021-10-22 14:56:09 +0530

    Add files via upload

---
+++ Spiritscape/Assets/Scripts/Controllers/GameplayController.cs
@@ -0,0 +1,230 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class GameplayController : MonoBehaviour {
+
+	public static GameplayController instance;
+	
+	[SerializeField]
+	private TextMeshProUGUI scoreText, endScore, bestScore, gameOverText;
+	
+	[SerializeField]
+	private Button restartGameButton, instructionsButton;
+	
+	[SerializeField]
+	private GameObject pausePanel;
+	
+	[SerializeField]
+	private GameObject[] spirits;
+	
+	[SerializeField]
+	private Sprite[] medals;
+	
+	[SerializeField]
+	private Image medalImage;
+
+	void Awake() {
+		MakeInstance ();
+		Time.timeScale = 0f;
+	}
+
+	void Start () {
+	
+	}
+	
+	void MakeInstance() {
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	public void PauseGame() {
+		if (SpiritScript.instance != null) {
+			if(SpiritScript.instance.isAlive) {
+				pausePanel.SetActive(true);
+				gameOverText.gameObject.SetActive(false);
+				endScore.text = "" + SpiritScript.instance.score;
+				bestScore.text = "" + GameController.instance.GetHighscore();
+				Time.timeScale = 0f;
+				restartGameButton.onClick.RemoveAllListeners();
+				restartGameButton.onClick.AddListener(() => ResumeGame());
+			}
+		}
+	}
+
+	public void GoToMenuButton() {
+		SceneFader.instance.FadeIn ("MainMenu");
+	}
+
+	public void ResumeGame() {
+		pausePanel.SetActive (false);
+		Time.timeScale = 1f;
+	}
+
+	public void RestartGame() {
+		SceneFader.instance.FadeIn (Application.loadedLevelName);
+	}
+
+	public void PlayGame() {
+		scoreText.gameObject.SetActive (true);
+		spirits [GameController.instance.GetSelectedSpirit ()].SetActive (true);
+		instructionsButton.gameObject.SetActive (false);
+		Time.timeScale = 1f;
+	}
+
+	public void SetScore(int score) {
+		scoreText.text = "" + score;
+	}
+
+	public void PlayerDiedShowScore(int score) {
+		pausePanel.SetActive (true);
+		gameOverText.gameObject.SetActive (true);
+		scoreText.gameObject.SetActive (false);
+
+		endScore.text = "" + score;
+
+		if (score > GameController.instance.GetHighscore ()) {
+			GameController.instance.SetHighscore(score);
+		}
+
+		bestScore.text = "" + GameController.instance.GetHighscore ();
+
+		if (score <= 20) {
+			medalImage.sprite = medals [0];
+		} else if (score > 20 && score < 40) {
+			medalImage.sprite = medals [1];
+
+			if (GameController.instance.IsGreenSpiritUnlocked () == 0) {
+				GameController.instance.UnlockGreenSpirit ();
+			}
+
+		} else {
+			medalImage.sprite = medals [2];
+			
+			if (GameController.instance.IsGreenSpiritUnlocked () == 0) {
+				GameController.instance.UnlockGreenSpirit ();
+			}
+
+			if (GameController.instance.IsYellowSpiritUnlocked () == 0) {
+				GameController.instance.UnlockYellowSpirit ();
+			}
+
+		}
+
+		restartGameButton.onClick.RemoveAllListeners ();
+		restartGameButton.onClick.AddListener (() => RestartGame());
+
+	}
+
+	public void ShareScore ()
+	{
+		pausePanel.SetActive (true);//show the panel
+		StartCoroutine ("TakeScreenShotAndShare");
+	}
+
+	IEnumerator TakeScreenShotAndShare ()
+	{
+		yield return new WaitForEndOfFrame ();
+
+		Texture2D tx = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24, false);
+		tx.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0);
+		tx.Apply ();
+
+		string path = Path.Combine (Application.temporaryCachePath, "sharedImage.png");//image name
+		File.WriteAllBytes (path, tx.EncodeToPNG ());
+
+		Destroy (tx); //to avoid memory leaks
+
+		new NativeShare ()
+			.AddFile (path)
+			.SetSubject ("This is my score")
+			.SetText ("share your score with your friends")
+			.Share ();
+
+
+		pausePanel.SetActive (false); //hide the panel
+		SceneFader.instance.FadeIn ("MainMenu");
+	}
+
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Spiritscape/Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	public static MenuController instance;
+
+	[SerializeField]
+	private GameObject[] spirits;
+
+	private bool isGreenSpiritUnlocked, isYellowSpiritUnlocked;
+
+	[SerializeField]
+	private Animator notificationAnim;
+	
+	[SerializeField]
+	private Text notificationText;
+		
+	void Awake() {
+		MakeInstance();
+	}
+
+	void Start() {
+		spirits [GameController.instance.GetSelectedSpirit ()].SetActive (true);
+		CheckIfSpiritsAreUnlocked ();
+	}
+
+	void MakeInstance() {
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	void CheckIfSpiritsAreUnlocked() {
+		if (GameController.instance.IsYellowSpiritUnlocked () == 1) {
+			isYellowSpiritUnlocked = true;
+		}
+
+		if (GameController.instance.IsGreenSpiritUnlocked () == 1) {
+			isGreenSpiritUnlocked = true;
+		}
+	}
+
+	public void PlayGame() {
+		SceneFader.instance.FadeIn ("Gameplay");
+	}
+	
+	public void ChangeSpirit() {
+
+		if (GameController.instance.GetSelectedSpirit()==0) {
+
+			if (isGreenSpiritUnlocked) {
+				spirits[0].SetActive(false);
+				GameController.instance.SetSelectedSpirit(1);
+				spirits[GameController.instance.GetSelectedSpirit()].SetActive(true);
+			}
+
+		} else if (GameController.instance.GetSelectedSpirit()==1) {
+
+			if (isYellowSpiritUnlocked) {
+
+				spirits[1].SetActive(false);
+				GameController.instance.SetSelectedSpirit(2);
+				spirits[GameController.instance.GetSelectedSpirit()].SetActive(true);
+
+			} else {
+
+				spirits[1].SetActive (false);
+				GameController.instance.SetSelectedSpirit(0);
+				spirits[GameController.instance.GetSelectedSpirit()].SetActive(true);
+
+			}
+
+		} else if (GameController.instance.GetSelectedSpirit()==2) {
+			spirits[2].SetActive(false);
+			GameController.instance.SetSelectedSpirit(0);
+			spirits[GameController.instance.GetSelectedSpirit()].SetActive(true);
+		}
+
+	}
+
+	public void NotificationMessage(string message)
+	 {
+		StartCoroutine (AnimateNotificationPanel(message));
+	}
+
+	IEnumerator AnimateNotificationPanel(string message) {
+		notificationAnim.Play("SlideIn");
+		notificationText.text = message;
+		yield return StartCoroutine(MyCoroutine.WaitForRealSeconds(2f));
+		notificationAnim.Play("SlideOut");
+	}
+
+	
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
